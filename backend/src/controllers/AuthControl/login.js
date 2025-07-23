2d77e03bfd9c52df51b6298636ca1fa2f61e60a1
Vasu Tayal <vasutayal1010@gmail.com>
2025-07-24 00:04:14 +0530

    Update login controller

--- backend/src/controllers/AuthControl/login.js
+++ backend/src/controllers/AuthControl/login.js
@@ -10,20 +10,25 @@ import {generateTokenAndSetCookie} from "../../../utils/generateTokenAndSetCooki
 export const login = async (req, res) => {
 	const { email, password } = req.body;
 	try {
+		//check if user exists
 		const user = await User.findOne({ email });
 		if (!user) {
 			return res.status(400).json({ success: false, message: "Invalid credentials" });
 		}
-		const isPasswordValid = await bcryptjs.compare(password, user.password);
-		if (!isPasswordValid) {
+
+		//Validate password
+		const isMatch = await bcryptjs.compare(password, user.password);
+		if (!isMatch) {
 			return res.status(400).json({ success: false, message: "Invalid credentials" });
 		}
-
+		
+		//generate token and set cookie
 		generateTokenAndSetCookie(res, user._id);
 
 		user.lastLogin = new Date();
 		await user.save();
 
+		//send success response
 		res.status(200).json({
 			success: true,
 			role: user.role,
@@ -34,8 +39,8 @@ export const login = async (req, res) => {
 			},
 		});
 	} catch (error) {
-		console.log("Error in Login ", error);
-		res.status(400).json({ success: false, message: error.message });
+		console.log("Error in Login ", error.message);
+		res.status(500).json({ success: false, message: "Server Error" });
 	}
 };
 
